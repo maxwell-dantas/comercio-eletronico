d690618a61b6aa69fa1b71492d82ad0ba90ccdaa
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-05-08 01:51:54 -0300

    feat: adiciona métodos de pausa e mensagem e remove limparConsole

--- src/comercioEletronico/view/Util.java
+++ src/comercioEletronico/view/Util.java
@@ -2,18 +2,28 @@ package comercioEletronico.view;
 
 public class Util {
 
-    // limpa os dados do console para melhor UX
-    public static void limparConsole() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
     // valida se a entrada é um número
     public static int lerInteiroSeguro(String entrada) {
         try {
             return Integer.parseInt(entrada);
-        }catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             return -1; // retorna -1 (valor para switch-case default)
         }
     }
+
+    public static void exibirmensagem(String mensagem) {
+        System.out.println("\n" + mensagem);
+    }
+
+    public static void pausar() {
+        System.out.println("\nPressione ENTER para continuar...");
+        try {
+            System.in.read();
+            while (System.in.available() > 0) {
+                System.in.read();
+            }
+        } catch (Exception e) {
+        }
+    }
 }
+
