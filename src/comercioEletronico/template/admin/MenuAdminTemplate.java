e90ba5235996f1d996625fc69582bd60d37ed538
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-05-08 23:36:04 -0300

    feat: implementa métodos de feedback

--- src/comercioEletronico/template/admin/MenuAdminTemplate.java
+++ src/comercioEletronico/template/admin/MenuAdminTemplate.java
@@ -25,14 +25,24 @@ public class MenuAdminTemplate {
     }
 
     public static void listarVendas(ArrayList<Venda> listaVendas) {
-        System.out.println("=== VENDAS CADASTRADAS ===\n");
+        System.out.println("\n=== VENDAS CADASTRADAS ===\n");
 
         if (listaVendas.isEmpty()) {
-            System.out.println("Nenhuma venda encontrada no momento.");
+            System.out.println("Nenhuma venda encontrada no momento.\n");
         } else {
             for (Venda venda : listaVendas) {
                 System.out.println(venda);
             }
         }
+    }
+
+    // MÉTODOS DE FEEDBACK
+
+    public static void exibirErroOpcaoInvalida() {
+        System.out.println("\nInsira um valor válido. Tente novamente!");
+    }
+
+    public static void exibirMensagemSaida() {
+        System.out.println("\nVoltando ao Sistema de Login...\n");
     }
 }
