6f6d1a25cee54c45ef4e4767e61fd330d8762388
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-05-10 00:31:38 -0300

    feat: modela a classe Main para rodar a aplicação

---
+++ src/comercioEletronico/view/Main.java
@@ -0,0 +1,10 @@
+package comercioEletronico.view;
+
+import comercioEletronico.view.visitante.VisitanteView;
+
+public class Main {
+    public static void main(String[] args) {
+        VisitanteView sistema = new VisitanteView();
+        sistema.exibirMenu();
+    }
+}
