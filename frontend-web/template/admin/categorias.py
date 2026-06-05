c3046f8e76325d2c7397f0ecc633b98f5384087f
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2026-06-05 02:22:07 -0300

    refactor(AdminCategoriaTemplate): modifica formatação de selectbox

--- frontend-web/template/admin/categorias.py
+++ frontend-web/template/admin/categorias.py
@@ -6,13 +6,16 @@ class AdminCategoriaTemplate(CrudGenerico):
     def __init__(self):
         super().__init__("Gerenciamento de Categorias", "categoria", "categorias")
 
+    # Override
     def formatar_selectbox(self, content):
-        return f"{content['id']} - {content['descricao']}"
+        return f"{content['descricao']}"
 
+    # Override
     def inserir(self):
         descricao = st.text_input("Descrição")
         return {"descricao": descricao}
 
+    # Override
     def atualizar(self, item_selecionado):
         descricao_up = st.text_input("Descrição", value=item_selecionado["descricao"])
         return {"descricao": descricao_up}
