12408e1f7406c6c77ee364cb056ff44500c72f6f
cazui <116229302+cazuisouto@users.noreply.github.com>
2026-06-10 15:21:16 -0300

    direcionando a parte do cliente no app.py

--- frontend-web/app.py
+++ frontend-web/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from template.admin.template import AdminTemplate
 from template.visitante.template import VisitanteTemplate
+from template.cliente.cliente_template import ClienteTemplate
 
 st.set_page_config(page_title="Mercadinho Caju", page_icon="🛒", layout="centered")
 
@@ -12,12 +13,12 @@ elif st.session_state.id_funcao == 1: # verifica se o usuário é admin
     AdminTemplate.renderizar_navegacao()
 
 elif st.session_state.id_funcao == 2:
-    # ClienteTemplate.renderizar_navegacao()
-    st.title(f"🛒 Bem-vindo, {st.session_state.nome_usuario}") # Placeholder para testes
-    st.write(f"Seu ID é: {st.session_state.id_usuario}")
-    if st.button("Sair"):
-        del st.session_state.id_usuario
-        st.rerun()
+    ClienteTemplate.renderizar_navegacao()
+    # st.title(f"🛒 Bem-vindo, {st.session_state.nome_usuario}") # Placeholder para testes
+    # st.write(f"Seu ID é: {st.session_state.id_usuario}")
+    # if st.button("Sair"):
+    #     del st.session_state.id_usuario
+    #     st.rerun()
 else:
     # EntregadorTemplate.renderizar_navegacao()
     st.title(f"🛒 Bem-vindo, {st.session_state.nome_usuario}!") # Placeholder para testes
--- frontend-web/template/cliente/cliente_template.py
+++ frontend-web/template/cliente/cliente_template.py
@@ -16,36 +16,44 @@ class ClienteTemplate:
         except Exception:
             return 500
     
-    #pag temporarias
+    # PÁGINAS TEMPORÁRIAS E DE REDIRECIONAMENTO 
+    
+    @staticmethod
+    def pagina_raiz():
+        
+        st.switch_page("template/cliente/catalogo_produtos.py")
+
+    @staticmethod
     def pagina_carrinho():
         st.header("🛒 Meu Carrinho")
         st.info("Página em construção! Aqui você verá os itens que selecionou.")
-
+    
+    @staticmethod
     def pagina_historico():
         st.header("🧾 Histórico de Compras")
         st.info("Página em construção! Suas compras anteriores serão listadas aqui.")
+    
+    # RENDERIZAÇÃO DO MENU
 
-    def redirecionar_para_produtos(pg_produtos):
-        # Esta função só serve para jogar o usuário para a URL /produtos
-        st.switch_page(pg_produtos)
-
-
+    @staticmethod
     def renderizar_navegacao():
 
-        # página raiz (default) invisível
+        # Página Raiz Invisível 
         pg_raiz = st.Page(
-            ClienteTemplate.redirecionar_para_produtos(pg_produtos()), 
+            ClienteTemplate.pagina_raiz, 
             title="Início", 
-            default=True 
+            default=True # É a página padrão, mas ninguém fica nela
         )
 
+        # Página de Produtos 
         pg_produtos = st.Page(
-            "catalogo_produtos.py", 
+            "template/cliente/catalogo_produtos.py", 
             title="Produtos Disponíveis", 
             icon="📦",
             url_path="produtos" 
         )
 
+        # páginas
         pg_carrinho = st.Page(
             ClienteTemplate.pagina_carrinho, 
             title="Meu Carrinho", 
@@ -54,23 +62,28 @@ class ClienteTemplate:
         )
 
         pg_historico = st.Page(
-            ClienteTemplate.pagina_historico(), 
+            ClienteTemplate.pagina_historico, 
             title="Histórico de Compras", 
             icon="🧾",
             url_path="historico"
         )
 
-
-        # NAVEGAÇÃO E SIDEBAR
-
-
-        menu_navegacao = st.navigation([pg_raiz, pg_produtos,  pg_carrinho, pg_historico], position="hidden")
+        # NAVEGAÇÃO E SIDEBAR (Incluindo a raiz na lista)
+        menu_navegacao = st.navigation(
+            [pg_raiz, pg_produtos, pg_carrinho, pg_historico], 
+            position="hidden"
+        )
 
         with st.sidebar:
-            st.header("Seja bem-vindo!")
+            st.header(f"Seja bem-vindo(a), {st.session_state.get('nome_usuario', '')}!")
             
+            # Os botões para o cliente clicar
             st.page_link(pg_produtos)
             st.page_link(pg_carrinho)
             st.page_link(pg_historico)
+
+            if st.button("Sair"):
+                del st.session_state.id_usuario
+                st.rerun()
 
         menu_navegacao.run()
